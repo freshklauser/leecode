95b819561fb2cd10e12bb55c71b6fb5b7642721b
sniky-lyu <junkklv@outlook.com>
2020-06-14 23:10:47 +0800

    bit

--- 1_DataStructure/BasicType/bit_operation.py
+++ 1_DataStructure/BasicType/bit_operation.py
@@ -157,6 +157,37 @@ def bin_watch(n):
     return res
 
 
+def sum_by_bin(a, b):
+    """
+    不使用 +, -, 实现整数 a+b （负数转化为正数再处理）
+    理解：
+        二进制加法：从以下可以看出，二进制加法不含进位部分就是异或^
+            0 + 0 = 0
+            0 + 1 = 1
+            1 + 0 = 1
+            1 + 1 = 0（进位 1）
+        a ^ b 得到了一个无进位加法结果
+        a & b 得到进位信息，但需左移1位才在高位
+    步骤：
+        a + b 的问题拆分为 (a 和 b 的无进位结果) + (a 和 b 的进位结果)
+        无进位加法使用异或运算计算得出
+        进位结果使用与运算和移位运算计算得出
+        循环此过程，直到进位为 0
+    """
+    a &= 0xFFFFFFFF
+    b &= 0xFFFFFFFF
+    while b:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1 & 0xFFFFFFFF
+        print(a, b)
+    return a if a < 0x80000000 else ~(a ^ 0xFFFFFFFF)
+
+
+res = sum_by_bin(10, -2)
+print(res)
+
+
 def count_prime_set_bits(low, high):
     """
     762. 二进制表示中质数个计算置位
--- 1_DataStructure/BasicType/reverse_bit.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author   : Administrator
-# @DateTime : 2020/6/14 18:29
-# @FileName : reverse_bit.py
-# @SoftWare : PyCharm
-
